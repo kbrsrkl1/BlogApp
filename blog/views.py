86d528c35bbdb485312f6f84239d82ea0bac3eb7
kbrsrkl1 <kubranur216@gmail.com>
2023-03-26 17:12:00 +0300

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Category, Blog
 from .serializers import BlogSerializer, CategorySerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAdminOrReadOnly
 
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class =  [IsAdminOrReadOnly]
     filterset_fields = ['name']
 
 
 class BlogView(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-    pagination_class = [IsAuthenticatedOrReadOnly]
+    permission_class = [IsAuthenticatedOrReadOnly]
     filterset_fields = ['category']
     search_fields =['title', 'content']
