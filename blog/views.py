8896cc59a4c7f2f3d05ce9d3fe183b2d34b0aebd
kbrsrkl1 <kubranur216@gmail.com>
2023-03-26 14:23:37 +0300

    commit

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,11 @@ from .serializers import BlogSerializer, CategorySerializer
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filterset_fields = ['name']
+
 
 class BlogView(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    filterset_fields = ['category']
     
